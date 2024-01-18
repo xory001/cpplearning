404e384d4a7191b3045429625e09b2e912911534
xory <xory.corp@outlook.com>
2024-01-18 12:23:29 +0800

    add xlog

---
+++ xlog/CXLog.cpp
@@ -0,0 +1,307 @@
+#include "pch.h"
+#include <iostream>
+#include <io.h>
+#include <string.h>
+#include <iomanip>
+#include "CXLog.h"
+
+
+#define DAY_MILLISECONDS  (24*60*60*1000)
+
+xlog::CLogMgr* xlog::CLogMgr::m_pInst = new xlog::CLogMgr;
+
+xlog::CLogMgr::CLogMgr()
+{
+    ::setlocale(LC_ALL, "zh-CN");
+    Init();
+}
+
+xlog::CLogMgr::~CLogMgr()
+{
+
+}
+
+void xlog::CLogMgr::Init()
+{
+    ClearHistoryLogFile();
+    InitializeCriticalSection(&m_cs);
+    time_t timeVal = time(NULL);
+    struct tm* tmVar = localtime(&timeVal);
+
+    for (int i = 0; i < X_LOG_LEVEL_MAX; i++)
+    {
+        m_arrLogCfg[i].logLevel = (X_LOG_LEVEL)i;
+        m_arrLogCfg[i].nCurrentDay = tmVar->tm_mday;
+#ifdef _DEBUG
+        m_arrLogCfg[i].bToStdOut = TRUE;
+#endif
+    }
+
+#ifndef _DEBUG
+    m_arrLogCfg[X_LOG_LEVEL_DEBUG].bToFile = FALSE;
+#endif
+
+}
+
+void xlog::CLogMgr::SetConfig(const std::string& strConfigFileName)
+{
+    EnterCriticalSection(&m_cs);
+
+    char szBuf[32] = { 0 };
+    std::string strSection;
+    for ( int i = 0; i < X_LOG_LEVEL_MAX; i++ )
+    {
+        strSection = Get_X_LOG_LEVEL_String( (X_LOG_LEVEL)i );
+        int nSize = GetPrivateProfileSectionA( strSection.c_str(), szBuf, 31, strConfigFileName.c_str() );
+        if ( nSize > 0 )
+        {
+            int nVal = GetPrivateProfileIntA( strSection.c_str(), "ToStdOut", -1, strConfigFileName.c_str());
+            if ( nVal >= 0 )
+            {
+                m_arrLogCfg[i].bToStdOut = nVal;
+            }
+
+            nVal = GetPrivateProfileIntA(strSection.c_str(), "ToFile", -1, strConfigFileName.c_str());
+            if ( nVal >= 0 )
+            {
+                m_arrLogCfg[i].bToFile = nVal;
+            }
+
+            nVal = GetPrivateProfileIntA(strSection.c_str(), "SeparateFile", -1, strConfigFileName.c_str());
+            if ( nVal >= 0 )
+            {
+                m_arrLogCfg[i].bSeparateFile = nVal;
+            }
+
+            nVal = GetPrivateProfileIntA(strSection.c_str(), "MaxFileSize", -1, strConfigFileName.c_str());
+            if ( nVal > 0 )
+            {
+                m_arrLogCfg[i].nMaxFileSize = 1024 * 1024 * nVal;
+            }
+
+            nVal = GetPrivateProfileIntA(strSection.c_str(), "MaxIndex", -1, strConfigFileName.c_str());
+            if ( nVal >= 0 )
+            {
+                m_arrLogCfg[i].nMaxIndex = nVal;
+            }
+
+            nVal = GetPrivateProfileIntA(strSection.c_str(), "KeepDays", -1, strConfigFileName.c_str());
+            if ( nVal > 0 )
+            {
+                m_arrLogCfg[i].nKeepDays = nVal;
+            }
+        }
+    }
+
+    LeaveCriticalSection(&m_cs);
+}
+
+ void xlog::CLogMgr::SetGenerateFileNameCallBack(X_LOG_LEVEL level, lpfnGenerateFileName fn)
+{
+
+}
+
+void xlog::CLogMgr::Write(X_LOG_LEVEL level, const std::string& strLog)
+{
+    EnterCriticalSection(&m_cs);
+    if ( m_arrLogCfg[level].bToStdOut && m_arrLogCfg[X_LOG_LEVEL_ALL].bToStdOut )
+    {
+        HWND hWndConsole = GetConsoleWindow();
+        if ( NULL != hWndConsole )
+        {
+            std::cout << strLog << std::endl;
+        }
+        else
+        {
+            OutputDebugStringA( strLog.c_str() );
+        }
+    }
+    if ( m_arrLogCfg[level].bToFile && m_arrLogCfg[X_LOG_LEVEL_ALL].bToFile )
+    {
+        Write2File(level, strLog);
+    }
+     
+    LeaveCriticalSection(&m_cs);
+}
+
+void xlog::CLogMgr::Write2File(X_LOG_LEVEL level, const std::string& strLog)
+{
+    if ( !m_arrLogCfg[level].bSeparateFile )
+    {
+        level = X_LOG_LEVEL_ALL;
+    }
+
+    CheckLogFile(level);
+
+    if ( m_arrLogCfg[level].pFile )
+    {
+        int nBytesWrite = fwrite(strLog.c_str(), 1, strLog.length(), m_arrLogCfg[level].pFile);
+        fflush(m_arrLogCfg[level].pFile);
+        m_arrLogCfg[level].nCurrentFileSize += nBytesWrite;
+        if (m_arrLogCfg[level].nCurrentFileSize > m_arrLogCfg[level].nMaxFileSize )
+        {
+            fclose(m_arrLogCfg[level].pFile);
+            m_arrLogCfg[level].nCurrentFileSize = 0;
+            m_arrLogCfg[level].pFile = NULL;
+        }
+    }
+}
+
+std::string xlog::CLogMgr::GenerateFileName(X_LOG_LEVEL level, int nCurrentIndex, int nMaxIndex, BOOL bSeparateFile)
+{
+    char szBuf[MAX_PATH] = { 0 };
+    int nLen = GetModuleFileNameA(NULL, szBuf, MAX_PATH);
+    if ( nLen > 4 )
+    {
+        szBuf[nLen - 4] = 0; //remove ".exe"
+    }
+
+    time_t timeVal = time(NULL);
+    struct tm* tmVar = localtime(&timeVal);
+    std::ostringstream oss;
+    oss << szBuf;
+
+    if ( nMaxIndex )
+    {
+        oss << "_" << GetCurrentProcessId();
+    }
+
+    if ( bSeparateFile )
+    {
+        oss << "_" << Get_X_LOG_LEVEL_String(level);
+    }
+    oss << "_" << tmVar->tm_year + 1900 << std::setw(2)<< std::setfill('0') <<  tmVar->tm_mon + 1 << std::setw(2) << std::setfill('0') << tmVar->tm_mday;
+    if ( nMaxIndex )
+    {
+        oss << "_" << nCurrentIndex;
+    }
+    oss << ".log";
+   
+    return oss.str();
+}
+
+void xlog::CLogMgr::CheckLogFile(X_LOG_LEVEL level)
+{
+    if (NULL == m_arrLogCfg[level].pFile)
+    {
+        OpenLogFile(level);
+    }
+    else
+    {
+        time_t timeVal = time(NULL);
+        struct tm* tmVar = localtime(&timeVal);
+        if (tmVar->tm_mday != m_arrLogCfg[level].nCurrentDay)
+        {
+            m_arrLogCfg[level].nCurrentDay = tmVar->tm_mday;
+            fclose(m_arrLogCfg[level].pFile);
+            m_arrLogCfg[level].pFile = NULL;
+            OpenLogFile(level);
+        }
+    }
+}
+
+void xlog::CLogMgr::OpenLogFile(X_LOG_LEVEL level)
+{
+    m_arrLogCfg[level].strFileName = GenerateFileName(level, m_arrLogCfg[level].nCurrentIndex,
+        m_arrLogCfg[level].nMaxIndex, m_arrLogCfg[level].bSeparateFile);
+
+    if ( ( 0 == m_arrLogCfg[level].nMaxIndex ) && ( 0 == _access( m_arrLogCfg[level].strFileName.c_str(), 0 ) ) )
+    {
+        std::string strBakFileName = m_arrLogCfg[level].strFileName + ".bak";
+        DeleteFileA(strBakFileName.c_str());
+        MoveFileA( m_arrLogCfg[level].strFileName.c_str(), strBakFileName.c_str() );
+    }
+   
+    if (m_arrLogCfg[level].nMaxIndex)
+    {
+        m_arrLogCfg[level].nCurrentIndex++;
+        if (m_arrLogCfg[level].nCurrentIndex > m_arrLogCfg[level].nMaxIndex)
+        {
+            m_arrLogCfg[level].nCurrentIndex = 0;
+        }
+    }
+
+    m_arrLogCfg[level].pFile = fopen(m_arrLogCfg[level].strFileName.c_str(), "w+");
+}
+
+void xlog::CLogMgr::ClearHistoryLogFile()
+{
+    char szAppName[32] = {0};
+    char szBuf[MAX_PATH] = {0};
+    int nSize = GetModuleFileNameA( NULL, szBuf, MAX_PATH  - 1 );
+    char* pszTrailingSlash = strrchr( szBuf, '\\' );
+    if ( pszTrailingSlash )
+    {
+        *pszTrailingSlash = 0;
+        pszTrailingSlash++;
+        int nBytesCopy = strlen( pszTrailingSlash ) > 31 ? 31 : strlen( pszTrailingSlash ); //app name  has ".exe"
+        strncpy( szAppName, pszTrailingSlash, nBytesCopy - 4 );
+    }
+
+    WIN32_FIND_DATAA wfd;
+    std::string strFindDir = szBuf;
+    strFindDir += "\\";
+    HANDLE hFind = FindFirstFileA( (strFindDir + "*").c_str() , &wfd );
+
+    if (INVALID_HANDLE_VALUE == hFind) 
+    {
+        return;
+    } 
+
+    // List all the files in the directory with some info about them.
+    UINT64 uDay4FILETIME = DAY_MILLISECONDS;
+    uDay4FILETIME *= (1000 * 10 );
+    SYSTEMTIME st;
+    GetLocalTime( &st );
+    FILETIME ftCurrent;
+    SystemTimeToFileTime( &st, &ftCurrent );
+    ULARGE_INTEGER  uTimeCurrent;
+    uTimeCurrent.LowPart = ftCurrent.dwLowDateTime;
+    uTimeCurrent.HighPart = ftCurrent.dwHighDateTime;
+
+    int nLenAppName = strlen( szAppName );
+    int nMinFileName = nLenAppName + 4; //4: the length of ".log"
+    do
+    {
+        if ( !( wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) )
+        {
+            int nLenFileName = strlen( wfd.cFileName );
+            if ( nLenFileName >= nMinFileName )
+            {
+                //{app name}*.log or .log.bak
+                if ( ( 0 == strncmp( szAppName, wfd.cFileName, nLenAppName ) ) )
+                {
+                    BOOL bLogFile = FALSE;
+                    if (  ( 0 == strncmp( wfd.cFileName + strlen( wfd.cFileName ) - 4, ".log", 4 ) )  )
+                    {
+                        bLogFile = TRUE;
+                    }
+                    else 
+                    {
+                        if ( nLenFileName >= ( nMinFileName + 4 ) )
+                        {
+                            if (  ( 0 == strncmp( wfd.cFileName + strlen( wfd.cFileName ) - 8, ".log.bak", 8 ) )  )
+                            {
+                                bLogFile = TRUE;
+                            }
+                        }
+                    }
+
+                    if ( bLogFile )
+                    {      
+                          ULARGE_INTEGER  uTime;
+                            uTime.LowPart = wfd.ftLastWriteTime.dwLowDateTime;
+                            uTime.HighPart = wfd.ftLastWriteTime.dwHighDateTime;
+                        if ( ( uTime.QuadPart + uDay4FILETIME * m_arrLogCfg[X_LOG_LEVEL_ALL].nKeepDays ) < uTimeCurrent.QuadPart ) // 7 days before
+                        {
+                            DeleteFileA( (strFindDir + wfd.cFileName  ).c_str() );
+                        }
+                    }
+                }
+            } //  if ( nLenFileName >= nLenAppName )
+        } // if ( !( wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) )
+    }
+    while ( FindNextFileA( hFind, &wfd ) );
+    FindClose(hFind);
+}
+
---
+++ xlog/CXLog.h
@@ -0,0 +1,302 @@
+#pragma once
+#include <sstream>
+#include <windows.h>
+#include <sys/timeb.h>
+#include <time.h>
+#include <iostream>
+#include <map>
+
+
+#ifdef DBASE_EXPORTS
+#define DBASE_EXPORT_API __declspec(dllexport)
+#else
+#define DBASE_EXPORT_API __declspec(dllimport)
+#endif
+
+/*  config file content
+; section: ALL, VERBOSE, DEBUG, INFO, WARN, ERROR, FATAL
+[ALL]
+; ToStdOut=1 or 0, default value: debug mode for console app set to 1, otherwise set to 0
+;ToStdOut=0
+; ToFile=1 or 0, default value: 1, but DEBUG info dropped in release mode
+ToFile=1
+; SeparateFile=1 or 0, default value: 0, set to 1 will create separate log file
+;SeparateFile=0
+; MaxFileSize=10, default value: 10, means 10MB
+;MaxFileSize=10
+;MaxIndex=0 or n; default value: 0, if > 0, the log file will from xx_0.log to xx_n.logand loop
+MaxIndex=1
+;KeepDays=1 or n; default value: 7
+KeepDays=7
+
+[DEBUG]
+; ToFile=1 or 0, default value: 1, but DEBUG info dropped in release mode
+ToFile=1
+*/
+
+namespace xlog
+{
+    //#define GET_ENUM_NAME(value) case value:return (#value);
+
+    typedef enum tagX_LOG_LEVEL
+    {
+        X_LOG_LEVEL_ALL = 0,
+        X_LOG_LEVEL_VERBOSE,
+        X_LOG_LEVEL_DEBUG,
+        X_LOG_LEVEL_INFO,
+        X_LOG_LEVEL_WARN,
+        X_LOG_LEVEL_ERROR,
+        X_LOG_LEVEL_FATAL,
+        X_LOG_LEVEL_MAX,
+    }X_LOG_LEVEL;
+
+    inline std::string Get_X_LOG_LEVEL_String(X_LOG_LEVEL val)
+    {
+        switch (val)
+        {
+        case X_LOG_LEVEL_ALL:
+            return "ALL";
+
+        case X_LOG_LEVEL_VERBOSE:
+            return "VERBOSE";
+
+        case X_LOG_LEVEL_DEBUG:
+            return "DEBUG";
+
+        case X_LOG_LEVEL_INFO:
+            return "INFO";
+
+        case X_LOG_LEVEL_WARN:
+            return "WARN";
+
+        case X_LOG_LEVEL_ERROR:
+            return "ERROR";
+
+        case X_LOG_LEVEL_FATAL:
+            return "FATAL";
+
+        default:
+            return "UNKNOWN";
+        }
+    }
+
+    typedef std::string(*lpfnGenerateFileName)( X_LOG_LEVEL logLevel, BOOL bSeparateFile );
+
+    class CLogCfg
+    {
+    public:
+        CLogCfg()
+        {
+            logLevel = X_LOG_LEVEL_DEBUG;
+            bToStdOut = FALSE;
+            bToFile = TRUE;
+            bSeparateFile = FALSE;  //if bSeparateFile == FALSE, write file to X_LOG_LEVEL_ALL.pFile
+            nMaxFileSize = 1024 * 1024 * 10; //10MB
+            nCurrentFileSize = 0;
+            nMaxIndex = 1;
+            nCurrentIndex  = 0;
+            pFile = NULL;
+            nCurrentDay = 0;
+            nKeepDays = 7;  //default, 7 days
+        }
+        virtual ~CLogCfg() {}
+
+    public:
+        X_LOG_LEVEL logLevel;
+        BOOL bToStdOut;
+        BOOL bToFile;
+        BOOL bSeparateFile;
+        int nMaxFileSize;
+        int nCurrentFileSize;
+        int nMaxIndex;
+        int nCurrentIndex;
+        int nCurrentDay;
+        int nKeepDays;      //keep log days
+        
+        //lpfnGenerateFileName fnGenFileName;
+        //std::string strFmt;
+        std::string strFileName;
+        FILE* pFile;
+    };
+
+    typedef std::map< X_LOG_LEVEL, CLogCfg > CMapLogCfg;
+    typedef CMapLogCfg::iterator CMapLogCfgIter;
+    typedef CMapLogCfg::const_iterator CMapLogCfgCIter;
+
+    /// <summary>
+    /// CWriterSink
+    /// </summary>
+     class CWriterSink
+     {
+     public:
+          virtual ~CWriterSink(){}
+
+     public:
+         DBASE_EXPORT_API virtual void Write(X_LOG_LEVEL level, const std::string& strLog) = 0;
+    };
+    
+    /// <summary>
+    /// CLogMgr
+    /// </summary>
+    class   CLogMgr : public CWriterSink
+    {
+    public:
+        ~CLogMgr();
+        DBASE_EXPORT_API static CLogMgr* Inst() { return m_pInst; }
+        //static void Release() {}
+    public:
+        DBASE_EXPORT_API virtual void Write(X_LOG_LEVEL level, const std::string& strLog);
+
+    public:
+        DBASE_EXPORT_API void SetConfig( const std::string& strConfigFileName );  //ansi format
+        DBASE_EXPORT_API void SetGenerateFileNameCallBack(X_LOG_LEVEL level, lpfnGenerateFileName fn );
+
+
+    private:
+        CLogMgr();
+        CLogMgr(const CLogMgr&) {}
+        CLogMgr& operator = (const CLogMgr&) { return *this; }
+    
+    private:
+        void Init();
+        void Write2File(X_LOG_LEVEL level, const std::string& strLog);
+        std::string GenerateFileName( X_LOG_LEVEL level, int nCurrentIndex, int nMaxIndex, BOOL bSeparateFile );
+        void CheckLogFile(X_LOG_LEVEL level);
+        void OpenLogFile(X_LOG_LEVEL level);
+        void ClearHistoryLogFile();
+
+    private:
+        static CLogMgr* m_pInst;
+        CRITICAL_SECTION m_cs;
+        CMapLogCfg m_mapLogCfg;
+        CLogCfg m_arrLogCfg[X_LOG_LEVEL_MAX];
+    };
+
+
+    /// <summary>
+    /// CWriter
+    /// </summary>
+    class CWriter
+    {
+    public:
+        CWriter( CWriterSink* pSink, X_LOG_LEVEL logLevel)
+        {
+            m_pSink = pSink;
+            ::setlocale(LC_ALL, "zh-CN");
+            BuildTime();
+            m_logLevel = logLevel;
+            m_ostream << "\t" << Get_X_LOG_LEVEL_String(logLevel) << "\t";
+        }
+
+        virtual ~CWriter()
+        {
+            m_ostream << std::endl;
+            if ( NULL != m_pSink )
+            {
+                m_pSink->Write( m_logLevel, m_ostream.str() );
+            }
+        }
+
+        template < typename T >
+        inline CWriter& operator << (const T& t)
+        {
+            m_ostream << t;
+            return *this;
+        }
+
+        template < typename T >
+        inline CWriter& operator << (T* t) {
+            m_ostream << "0x" << t;
+            return *this;
+        }
+
+        template <>
+        inline CWriter& operator << (char* lpInfo)
+        {
+            return operator << ((const char*)lpInfo);
+        }
+
+        template <>
+        inline CWriter& operator << (const char* lpInfo)
+        {
+            m_ostream << lpInfo;
+            return *this;
+        }
+
+        template <>
+        inline CWriter& operator << (const std::wstring& strInfo)
+        {
+            return operator << (strInfo.c_str());
+        }
+
+        template <>
+        inline CWriter& operator << (const wchar_t* lpwInfo)
+        {
+            int nLen = wcslen(lpwInfo) + 1;
+            int nBytes = ::WideCharToMultiByte(CP_ACP, 0, lpwInfo, nLen, NULL, 0, NULL, NULL);
+            char* szBuf = new char[nBytes];
+            memset(szBuf, 0, nBytes);
+            ::WideCharToMultiByte(CP_ACP, 0, lpwInfo, nLen, szBuf, nBytes, NULL, NULL);
+            m_ostream << szBuf;
+            delete[]szBuf;
+            return *this;
+        }
+
+        template <>
+        inline CWriter& operator << (wchar_t* lpwInfo)
+        {
+            return operator << ((const wchar_t*)lpwInfo);
+        }
+
+#ifdef _AFX
+        template <>
+        inline CWriter& operator << (const CString& strInfo)
+        {
+            return operator << ((LPCTSTR)strInfo);
+        }
+#endif
+
+    protected:
+        void BuildTime()
+        {
+            struct _timeb timeb;
+            char szTimeBuf[32] = { 0 };
+            _ftime_s(&timeb);
+            struct tm local;
+            localtime_s(&local, &timeb.time);
+            sprintf_s(szTimeBuf, "%04d.%02d.%02d %02d:%02d:%02d.%03d", local.tm_year + 1900, local.tm_mon + 1,
+                local.tm_mday, local.tm_hour, local.tm_min, local.tm_sec, timeb.millitm);
+            m_ostream << szTimeBuf;
+        }
+
+    protected:
+        std::ostringstream m_ostream;
+        X_LOG_LEVEL m_logLevel;
+        CWriterSink* m_pSink;
+    };
+}
+
+
+#define XWRITE_ALL_THREAD_INFO "[t=" << GetCurrentThreadId() << "]"
+#define XWRITE_ALL_LINE_INFO( str )  "[" << __FUNCTION__ << ":" << __LINE__ << "][" << str  << "]"
+
+//use macro to output log
+#define XWRITE_VERBOSE xlog::CWriter( xlog::CLogMgr::Inst(), xlog::X_LOG_LEVEL_VERBOSE ) << XWRITE_ALL_THREAD_INFO
+#define XWRITE_DEBUG xlog::CWriter( xlog::CLogMgr::Inst(), xlog::X_LOG_LEVEL_DEBUG ) << XWRITE_ALL_THREAD_INFO
+#define XWRITE_INFO xlog::CWriter( xlog::CLogMgr::Inst(), xlog::X_LOG_LEVEL_INFO ) << XWRITE_ALL_THREAD_INFO
+#define XWRITE_WARNING xlog::CWriter( xlog::CLogMgr::Inst(), xlog::X_LOG_LEVEL_WARN ) << XWRITE_ALL_THREAD_INFO
+#define XWRITE_ERROR xlog::CWriter( xlog::CLogMgr::Inst(), xlog::X_LOG_LEVEL_ERROR ) << XWRITE_ALL_THREAD_INFO
+#define XWRITE_FATAL xlog::CWriter( xlog::CLogMgr::Inst(), xlog::X_LOG_LEVEL_FATAL ) << XWRITE_ALL_THREAD_INFO
+
+#define TRACE_VERBOSE( str )			XWRITE_VERBOSE << XWRITE_ALL_LINE_INFO( str )
+#define TRACE_VERBOSE_THIS( str )		XWRITE_VERBOSE << "[this=" << this <<"]" << XWRITE_ALL_LINE_INFO( str )
+#define TRACE_DEBUG( str )				XWRITE_DEBUG << XWRITE_ALL_LINE_INFO( str )
+#define TRACE_DEBUG_THIS( str )		XWRITE_DEBUG << "[this=" << this <<"]" << XWRITE_ALL_LINE_INFO( str )
+#define TRACE_INFO( str )				XWRITE_INFO << XWRITE_ALL_LINE_INFO( str )
+#define TRACE_INFO_THIS( str )		    XWRITE_INFO << "[this=" << this <<"]" << XWRITE_ALL_LINE_INFO( str )
+#define TRACE_WARNING( str )			XWRITE_WARNING << XWRITE_ALL_LINE_INFO( str )
+#define TRACE_WARNING_THIS( str )		XWRITE_WARNING << "[this=" << this <<"]" << XWRITE_ALL_LINE_INFO( str )
+#define TRACE_ERR( str )				XWRITE_ERROR << XWRITE_ALL_LINE_INFO( str )
+#define TRACE_ERR_THIS( str )		    XWRITE_ERROR << "[this=" << this <<"]" << XWRITE_ALL_LINE_INFO( str )
+#define TRACE_FATAL( str )				XWRITE_FATAL << XWRITE_ALL_LINE_INFO( str )
+#define TRACE_FATAL_THIS( str )		XWRITE_FATAL << "[this=" << this <<"]" << XWRITE_ALL_LINE_INFO( str )
